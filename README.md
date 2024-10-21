9a8fc90e643b18de7388e7a5cdacd9156d4de6ab
samimnif <samimnif@icloud.com>
2024-10-21 17:09:12 -0400

    DHT11 sensor Getter Function

--- README.md
+++ README.md
@@ -8,3 +8,12 @@
 > To activate the environment, run the following cmd `source gmsenv/bin/activate` 
 
 ## Project Folder Tree
+
+### DHT11 Library Install:
+```
+git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+sudo apt-get install python3-setuptools
+sudo apt-get install python3-dev
+cd Adafruit_Python_DHT
+sudo python setup.py install --force-pi
+```
--- gms.py
+++ gms.py
@@ -1,15 +1,26 @@
 import yaml
+import sys
 import RPi.GPIO as GPIO
 import time
+import adafruit_dht
+import board
+#import Adafruit_DHT
 
 with open("config.yaml") as f:
     cfg = yaml.load(f, Loader=yaml.FullLoader)
 
 class GMS:
     def __init__(self):
-        GPIO.setmode(GPIO.BOARD)
-        self.dht = cfg["Temp&Hum"]["SIG"]
+        #GPIO Mode BCM - GPIO numbering
+        dht_pin = cfg["Temp&Hum"]["SIG"]
+        self.dht= adafruit_dht.DHT11(getattr(board, dht_pin))
+    def get_temp_hum(self):
+        #humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, self.dht)
+        temperature = self.dht.temperature
+        humidity = self.dht.humidity
+        return humidity, temperature
 
 if __name__ == "__main__":
-    gms1 = GMS()
     print(cfg)
+    gms1 = GMS()
+    print(gms1.get_temp_hum())
